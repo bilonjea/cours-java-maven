bdfe676df7a78d5c7d76985e8257cbdc3b24ed93
bilonjea <j.bilong@gmail.com>
2020-07-30 01:34:46 +0200

    packaging the app

---
+++ src/main/java/com/test/freme/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test.freme;
+
+/**
+ * @author bilonjea
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		 MySQLAccess dao = new MySQLAccess();
+	     dao.readDataBase();
+
+	}
+
+}
