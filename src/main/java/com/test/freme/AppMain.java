84ddf5236e09f3c3b898d96ed3d4444b6a1e2a29
abxlayedev <67095076+abxlayedev@users.noreply.github.com>
2020-07-30 15:45:44 +0200

    push abdou branch

---
+++ src/main/java/com/test/freme/AppMain.java
@@ -0,0 +1,49 @@
+package com.test.freme;
+
+import org.apache.log4j.Logger;
+
+import com.test.freme.model.Individu;
+import com.test.freme.model.Person;
+
+import lombok.extern.log4j.Log4j;
+
+// @author bilonjea
+@Log4j
+public class AppMain {
+
+    //@param args
+
+    public static void main(String[] args) {
+        //log.info("Entrée de la méthode main");
+        log.debug("Entree dans la méthode main");
+
+        log.info("Création Personne");
+        Person p = new Person();
+        p.setAdress("19 Rue Rochebrune 93100");
+        p.setAge(18);
+        p.setNom("Aferyat");
+        p.setPrenom("Aharone");
+        System.out.println(p.toString());
+        log.info("Person"+p.toString());
+
+        log.info("Création Individu");
+        Individu individu = Individu.builder().build();
+        individu.setAdress("19 Rue Rochebrune1 93100");
+        individu.setAge(19);
+        individu.setNom("Aferyat1");
+        individu.setPrenom("Aharone1");
+        System.out.println(individu.toString());
+        log.info("Individu"+individu.toString());
+
+        log.info("Création Individu2");
+        Individu individu2 = Individu.builder()
+                .nom("Aferyat2")
+                .prenom("Aharone2")
+                .age(20)
+                .adress("19 Rue Rochebrune2 93100")
+                .build();
+        log.info("Individu2: "+ individu2.toString());
+
+    }
+
+}
---
+++ src/main/java/com/test/freme/model/Individu.java
@@ -0,0 +1,16 @@
+package com.test.freme.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Individu {
+
+    private String nom;
+    private String prenom;
+    private int age;
+    private String adress;
+}
